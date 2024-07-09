c5b3b2e5c198a26fb7b420be21300b259b26cac3
pooriya <adibradp@gmail.com>
2024-07-09 17:10:09 +0330

    this is for test

--- test1/login/urls.py
+++ test1/login/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('Login/', views.Login, name='login')
+    path('Login/', views.Login, name='login'),
+    path('test/', views.test, name='test'),
 ]
--- test1/login/views.py
+++ test1/login/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def Login(request):
     return render(request, 'index.html')
+def test(request):
+    return render(request, 'test.html')
