63c29e7a58199c21ef2ccacd786cb9aab769084c
pooriya <adibradp@gmail.com>
2024-07-10 17:30:46 +0330

    in this scope we teach Crud in django and create with bootstrap

--- test1/login/urls.py
+++ test1/login/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('Login/', views.Login, name='login'),
     path('test/', views.test, name='test'),
+    path('create/', views.create, name='create'),
 ]
--- test1/login/views.py
+++ test1/login/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . import models
 # Create your views here.
 def Login(request):
@@ -6,16 +6,28 @@ def Login(request):
     #register
     product1=models.product(name='noshabe',price=100,quantity=100)
     product1.save()
+
     #update
     product.name='sanevich'
     product.price=250.25
     product.quantity=560
     product.save()
-    """
-    products=models.product.objects.all()
-
     product=models.product.objects.filter(id=3).first()
-    product.delete()
+
+
+
+    for product in products:
+        product.delete()
+   """
+    products = models.product.objects.all()
     return render(request, 'index.html',context={'products':products})
 def test(request):
     return render(request, 'test.html')
+def create(request):
+    if request.method == 'GET':
+        name = request.GET.get('name')
+        price = request.GET.get('price')
+        quantity = request.GET.get('quantity')
+        product=models.product(name=name,price=price,quantity=quantity)
+        product.save()
+        return redirect('login')
