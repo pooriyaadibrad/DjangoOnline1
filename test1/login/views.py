f0a5e0787f72edd85817fdbfa274eff5874c5a91
pooriya <adibradp@gmail.com>
2024-07-10 17:06:52 +0330

    crud be complete

--- test1/login/views.py
+++ test1/login/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 from . import models
 # Create your views here.
 def Login(request):
+    """
+    #register
     product1=models.product(name='noshabe',price=100,quantity=100)
     product1.save()
-    return render(request, 'index.html')
+    #update
+    product.name='sanevich'
+    product.price=250.25
+    product.quantity=560
+    product.save()
+    """
+    products=models.product.objects.all()
+
+    product=models.product.objects.filter(id=3).first()
+    product.delete()
+    return render(request, 'index.html',context={'products':products})
 def test(request):
     return render(request, 'test.html')
