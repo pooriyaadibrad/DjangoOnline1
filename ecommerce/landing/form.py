23fb841e7572730600e130eaf002761a55f6f819
pooriya <adibradp@gmail.com>
2024-07-21 17:41:48 +0330

    for trating

--- ecommerce/landing/form.py
+++ ecommerce/landing/form.py
@@ -1,10 +1,11 @@
 from django import forms
+from . import models
 
-
-class LandingForm(forms.Form):
+class LandingForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(LandingForm, self).__init__(*args, **kwargs)
         for item in LandingForm.visible_fields(self):
             item.field.widget.attrs['class'] = 'form-control'
-    name=forms.CharField(max_length=100)
-    age=forms.IntegerField()
+    class Meta:
+        model = models.message
+        fields = '__all__'
